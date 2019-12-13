300203604b2abe23f6c53633e7cc4a21675def4c
evgeniyamosunova <56960146+evgeniyamosunova@users.noreply.github.com>
2019-12-13 04:14:02 +0300

    Add files via upload

---
+++ lab_6/lab_6/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        Car c1 = new Car("kia", "ceed", "sw", 5220000, 2009);
+        Car c2 = new Car("Lada", "Granta", "Cross", 40000, 1999);
+        Car c3 = new Car("Shkoda", "JX", "Standart", 230000, 2012);
+        c1.println();
+        c2.println();
+        c3.println();
+    }
+}
+
